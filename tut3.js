f187e278bff45331d57cb6bd5cef8bd6ebbc6e17
Afreenkhan2019 <103169448+Afreenkhan2019@users.noreply.github.com>
2022-06-15 17:19:37 +0530

    Add files via upload

---
+++ tut3.js
@@ -0,0 +1,25 @@
+console.log("Tut 3");
+
+class user{
+
+    constructor(name){
+        this.nane= name;
+    }
+
+SayHi(){
+    alert(this.nane)
+}
+
+}
+
+//Usage
+let abc = new user('Afreen'); 
+abc.SayHi();
+
+
+
+
+
+
+
+
